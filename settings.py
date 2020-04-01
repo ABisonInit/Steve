9293d4db0b4c24943405be5454ff60f8f50af92a
ABisonInit <61840972+ABisonInit@users.noreply.github.com>
2020-04-01 00:25:19 -0400

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,21 +1,30 @@
 import os
 import pygame
-#defining variables with Bison Dos
-white = (255,255,255)
-black = (0,0,0)
-navyblue = (0,2,35)
 
-#game settings
+# defining variables with Bison Dos
+white = (255, 255, 255)
+black = (0, 0, 0)
+navyblue = (0, 2, 35)
+lightgrey = (169, 169, 169)
+
+# game settings
 width = 1024
 height = 768
 fps = 60
 title = "Steve"
 BgColour = navyblue
 
-tilesize = 16
-gridwidth = width/tilesize
-gridheight = height/tilesize
+tilesize = 32
+gridwidth = width / tilesize
+gridheight = height / tilesize
 
-#assets folder
+# assets folder
 gameFolder = os.path.dirname(__file__)
 senseiFolder = os.path.join(gameFolder, "img/generic-rpg-pack_v0.3_(alpha-release)_vacaroxa/rpg-pack/chars/sensei")
+
+# loadingScreen
+x_steve = 5
+y_steve = 5
+
+# Steve
+steveSpeed = 1
